2811801f3b8ac15ce22e79b814fd14b7562a5f36
ShuichiIshida <s_ishida@leading-solutions.co.jp>
2023-11-17 09:25:14 +0900

    課題「関数をMDNで調べて使ってみよう」を提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,15 @@
+//本日の日付を取得
+const date = new Date();
+
+// 年を取得
+const year = date.getFullYear();
+
+// 月を取得
+
+const month = date.getMonth();
+
+//日を取得
+const day = date.getDate();
+
+// 年月日を出力
+console.log(year + '年' + month + '月' + day + '日');
